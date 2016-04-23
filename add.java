06ff5e2e9bd9b75a39d168bc44f7d5773b1aa3ea
chanda-gogi <chandagogi6@gmail.com>
2016-04-23 14:21:10 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,8 +1,10 @@
 class Addition{
 public static void main(String [] args){
 int a=5;
-int b=23, c;
+int b=23, c,d;
 c=a+b;
+d=b-c;
 System.out.println("Addition="+c);
+System.out.println("subtraction="+d);
 }
 }
